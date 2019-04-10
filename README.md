740d8940c93426c2b347aec53efd647e32923fbb
Matthew Rademaekers <mmr@yottadb.com>
2019-04-10 15:51:34 -0400

    Added comments, Resolved memory bug.
    Added comments throughout the code base.
    Updated the .gitignore to include *.png
    Resolved memory bug (bug was nonexistant)
    Added note about memory management to the README

--- README.md
+++ README.md
@@ -3,17 +3,26 @@
 My fractal program ported to C
 
 # Dependacies
-##tinypngout
+## TinyPngOut
 https://www.nayuki.io/page/tiny-png-output
 Used to write the png out
 
 # To Compile
 At the moment this program is designed to be recompiled on settings changes
 I don't know if this will change in the future since it is a small program
+## A note on memory
+Since this program allocates the array of iterations (floats) at compile time.
+The minimium memory requirements will be dominated by 4 * imageHeight * imageWidth bytes.
+This is a small enough footprint for most purposes so don't worry about it.
+However if you allocate more memory then your system has it will immedately segfault.
+But and issue arises when you allocate nearly all your memory. C only reports memory usage as it writes to the array.
+So the program memory usage will slowly increase over time. If the system runs out of memory then it will crash.
+Meaning, if the program needs 7GB to run, and your system has 8GB (total including swap space). And you are currently using 2GB base.
+You will crash. So give the program a little breathing room.
 
 # The Roadmap
 I plan on adding several features over time. Caching progress to the disk, threading, gpu computing cheifly amoung them for now.
-Starting at version 2.0 I will be maintaining 2 version. One that uses the database Yottadb for disk caching
+Starting at version 2.0 I will be maintaining 2 version. One that uses the database Yottadb for disk caching,
 and another that writes to the disk it's self. I anticipate that the database version will be faster however will require
 it to be installed.
 After these features are done I will add zoom fucntionality
@@ -35,6 +44,3 @@ gpu computing
 
 ## 5.0
 Produce zoom videos
-
-# Known Bugs
-Segfualt on large images where none should happen. Issue is likely with the TinyPngOut image data being created on the stack
--- mandelbrot.c
+++ mandelbrot.c
@@ -25,13 +25,20 @@
 #define depth 1
 #endif
 
-const float logOf2 = log2l(2);
+const float logOf2 = log2l(2); //constant so we only compute this once
+
+/* we declare this here because in theory allocating this at compile time
+ * is slightly faster. In practice this really wont be true due to caching
+ * tests will be done later to see if there is an appreciable difference
+ */
+/* additionally I am using floats here since the accuracy isn't super important for right now
+ * in the future this might change when doing zoom's however that will drastically increase memory usage
+ */
 float pixels[setwidth][setheight];
 
-
 typedef struct compNum {
-		long double r;
-		long double i;
+	long double r;
+	long double i;
 } compNum;
 
 compNum calcC(int x, int y);
@@ -44,145 +51,162 @@ static int printError(enum TinyPngOut_Status status);
 
 
 int main(){
+	printf("%lu\n", sizeof(pixels));
 
-		long numPixels = setheight * setwidth;
-		long numDone = 0;
-		float percent = 0;
-		//array to map the co-ord plane to;
-		for(int x = 0; x < setwidth; ++x){
-				for(int y = 0; y < setheight; ++y){
-						pixels[x][y] = calcPoint(calcC(x, y));
-						++numDone;
-						float newPercent = ((float) numDone / (float) numPixels) * 100;
-						newPercent = trunc(newPercent);
-						if(percent < newPercent){
-								percent = newPercent;
-								printf("%.0f%% done. %ld/%ld pixels.\n", percent, numDone, numPixels);
-						}
-				}
-
-		}
-
-		return makePicture();
-}
-
-compNum calcC(int x, int y){
-		return (compNum) {((4.0L/(setwidth - 1)) * x - 2.5f) * depth + xShift, 
-				         (((9.0L /4.0L)/(setheight - 1)) * y - (9.0l / 8.0l)) * depth - yShift};
-}
-
-float calcPoint(compNum point){
-		long double x = 0, y = 0, xSqr = 0, ySqr = 0;
-		int iteration = 0; 
-
-		while( xSqr + ySqr < 1000 && iteration < max){
-				long double xTemp = xSqr - ySqr + point.r;
-				y = 2 * x * y + point.i;
-				x = xTemp;
-				xSqr = x * x;
-				ySqr = y * y;
-				++iteration;
-		}
-
-		if(iteration == max){
-				return iteration;
-		} else{
-				float nu = log2(log2(sqrt(xSqr + ySqr))) / logOf2;
-				return iteration + 1 - nu;
-		}
-}
-
-int makePicture(){
-		char *fileName = (char*) malloc(80 * sizeof(char));
-		sprintf(fileName, "Mandelbrot%dx%dat%d.png", setwidth, setheight, max);
-		FILE *fout = fopen(fileName, "wb");
-		if(fout == NULL){
-				perror("Error: fopen");
-				return EXIT_FAILURE;
-		}
-
-		struct TinyPngOut pngout;
-		enum TinyPngOut_Status status = TinyPngOut_init(&pngout, (uint32_t)setwidth, (uint32_t)setheight, fout);
-		if(status != TINYPNGOUT_OK)
-				return printError(status);
-
+	long numPixels = (long)setheight * (long)setwidth;
+	long numDone = 0;
+	float percent = 0;
+	//array to map the co-ord plane to;
+	for(int x = 0; x < setwidth; ++x){
 		for(int y = 0; y < setheight; ++y){
-				for(int x = 0; x < setwidth; ++x){
-						status = TinyPngOut_write(&pngout, getColor(pixels[x][y]), (size_t) 1);
-						if (status != TINYPNGOUT_OK)
-								return printError(status);
-				}
+			pixels[x][y] = calcPoint(calcC(x, y));
+			++numDone;
+			float newPercent = ((float) numDone / (float) numPixels) * 100;
+			newPercent = trunc(newPercent);
+			if(percent < newPercent){
+				percent = newPercent;
+				printf("%.0f%% done. %ld/%ld pixels.\n", percent, numDone, numPixels);
+			}
 		}
-		if (fclose(fout) != 0) {
-				perror("Error: fclose");
-				return EXIT_FAILURE;
-		}
-		return EXIT_SUCCESS;
+
+	}
+
+	printf("\nDone calculating. Creating the image now.\n");
+	return makePicture();
 }
 
-uint8_t* getColor(float iterations){
-		static uint8_t color[3];
-		if (iterations == max){
-				color[0] = 0x00;
-				color[1] = 0x00;
-				color[2] = 0x00;
-		} else{
-				float hLow = fmod((360.0f / (max - 1)) * floor(iterations) + 271, 360);
-				float hHigh = fmod((360.0f / (max - 1)) * ceil(iterations) + 271, 360);
-				float percent = fmod(iterations, 1);
-				
-				float h = (hHigh - hLow) * percent + hLow;
-				float s = 1.0f;
-				float l = (iterations / (iterations + 4.0f)) / 1.75f;
-				//float l = 0.5;
-
-				hslTorgb(color, h, s, l);
-		}
-		return color;
+/* This takes a point in the image
+ * and returns the complex number associated with this
+ */
+compNum calcC(int x, int y){
+	return (compNum) {((4.0L/(setwidth - 1)) * x - 2.5f) * depth + xShift, 
+		         (((9.0L /4.0L)/(setheight - 1)) * y - (9.0l / 8.0l)) * depth - yShift};
 }
 
-void hslTorgb(uint8_t *color, float h, float s, float l){
-		float c = (1 - fabs(2 * l - 1)) * s;
-		float x = c * (1 - fabs(fmod((h / 60.0f), 2.0f) - 1));
-		float m = l - (c / 2.0f);
+/* This takes a complex point and calculates the fractal at it
+ */
+float calcPoint(compNum point){
+	long double x = 0, y = 0, xSqr = 0, ySqr = 0;
+	int iteration = 0; 
 
-		if (h >= 0 && h < 60){
-				color[0] = (uint8_t) ((c + m) * 255);
-				color[1] = (uint8_t) ((x + m) * 255);
-				color[2] = (uint8_t) (m * 255);
-		} else if (h >= 60 && h < 120){
-				color[0] = (uint8_t) ((x + m) * 255);
-				color[1] = (uint8_t) ((c + m) * 255);
-				color[2] = (uint8_t) (m * 255);
-		} else if (h >= 120 && h < 180){
-				color[0] = (uint8_t) (m * 255);
-				color[1] = (uint8_t) ((c + m) * 255);
-				color[2] = (uint8_t) ((x + m) * 255);
-		} else if (h >= 180 && h < 240){
-				color[0] = (uint8_t) (m * 255);
-				color[1] = (uint8_t) ((x + m) * 255);
-				color[2] = (uint8_t) ((c + m) * 255);
-		} else if (h >= 240 && h < 300){
-				color[0] = (uint8_t) ((x + m) * 255);
-				color[1] = (uint8_t) (m * 255);
-				color[2] = (uint8_t) ((c + m) * 255);
-		} else if (h >= 300 && h < 360){
-				color[0] = (uint8_t) ((c + m) * 255);
-				color[1] = (uint8_t) (m * 255);
-				color[2] = (uint8_t) ((x + m) * 255);
-		}
+	while( xSqr + ySqr < 1000 && iteration < max){
+		long double xTemp = xSqr - ySqr + point.r;
+		y = 2 * x * y + point.i;
+		x = xTemp;
+		xSqr = x * x;
+		ySqr = y * y;
+		++iteration;
+	}
+
+	if(iteration == max){
+		return iteration; //if it's in the set no need for normalization
+	} else{
+		float nu = log2(log2(sqrt(xSqr + ySqr))) / logOf2; //normalization
+		return iteration + 1 - nu;
+	}
 }
-
-
-static int printError(enum TinyPngOut_Status status){
-		const char *msg;
-		switch (status){
-				case TINYPNGOUT_OK					:	msg = "OK";					break;
-				case TINYPNGOUT_INVALID_ARGUMENT	:	msg = "Invalid argument";	break;
-				case TINYPNGOUT_IMAGE_TOO_LARGE		:	msg = "Image too large";	break;
-				case TINYPNGOUT_IO_ERROR			:	msg = "I/O error";			break;
-				default								:	msg = "Unknown error";		break;
-		}
-		fprintf(stderr, "Error: %s\n", msg);
+/* This takes list of pixels and writes it to a file
+ * given the speed of writing the image (compared to the calculation)
+ * there will be no ongoing progress update
+ */
+int makePicture(){
+	char *fileName = (char*) malloc(80 * sizeof(char));
+	sprintf(fileName, "Mandelbrot%dx%dat%d.png", setwidth, setheight, max);
+	FILE *fout = fopen(fileName, "wb");
+	if(fout == NULL){
+		perror("Error: fopen");
 		return EXIT_FAILURE;
+	}
+	/* whats nice about this is that it doesn't store the picture in memory */
+	struct TinyPngOut pngout;
+	enum TinyPngOut_Status status = TinyPngOut_init(&pngout, (uint32_t)setwidth, (uint32_t)setheight, fout);
+	if(status != TINYPNGOUT_OK)
+		return printError(status);
+	for(int y = 0; y < setheight; ++y){
+		for(int x = 0; x < setwidth; ++x){
+			status = TinyPngOut_write(&pngout, getColor(pixels[x][y]), (size_t) 1);
+			if (status != TINYPNGOUT_OK)
+				return printError(status);
+		}
+	}
+	if (fclose(fout) != 0) {
+		perror("Error: fclose");
+		return EXIT_FAILURE;
+	}
+	return EXIT_SUCCESS;
+}
+
+/* This is iterative coloring
+ * it takes a number of iterations and returns
+ * the color for that value
+ * it isn't great since the color becomes increasingly similar at higher iterations
+ */
+uint8_t* getColor(float iterations){
+	static uint8_t color[3];
+	if (iterations == max){
+		color[0] = 0x00;
+		color[1] = 0x00;
+		color[2] = 0x00;
+	} else{
+		float hLow = fmod((360.0f / (max - 1)) * floor(iterations) + 271, 360);
+		float hHigh = fmod((360.0f / (max - 1)) * ceil(iterations) + 271, 360);
+		float percent = fmod(iterations, 1);
+		
+		float h = (hHigh - hLow) * percent + hLow;
+		float s = 1.0f;
+		float l = (iterations / (iterations + 4.0f)) / 1.75f;
+		//float l = 0.5;
+
+		hslTorgb(color, h, s, l);
+	}
+	return color;
+}
+
+/* a pretty terrible hslTorgb function
+ * this could probably be updated to use switch/case
+ */
+void hslTorgb(uint8_t *color, float h, float s, float l){
+	float c = (1 - fabs(2 * l - 1)) * s;
+	float x = c * (1 - fabs(fmod((h / 60.0f), 2.0f) - 1));
+	float m = l - (c / 2.0f);
+
+	if (h >= 0 && h < 60){
+		color[0] = (uint8_t) ((c + m) * 255);
+		color[1] = (uint8_t) ((x + m) * 255);
+		color[2] = (uint8_t) (m * 255);
+	} else if (h >= 60 && h < 120){
+		color[0] = (uint8_t) ((x + m) * 255);
+		color[1] = (uint8_t) ((c + m) * 255);
+		color[2] = (uint8_t) (m * 255);
+	} else if (h >= 120 && h < 180){
+		color[0] = (uint8_t) (m * 255);
+		color[1] = (uint8_t) ((c + m) * 255);
+		color[2] = (uint8_t) ((x + m) * 255);
+	} else if (h >= 180 && h < 240){
+		color[0] = (uint8_t) (m * 255);
+		color[1] = (uint8_t) ((x + m) * 255);
+		color[2] = (uint8_t) ((c + m) * 255);
+	} else if (h >= 240 && h < 300){
+		color[0] = (uint8_t) ((x + m) * 255);
+		color[1] = (uint8_t) (m * 255);
+		color[2] = (uint8_t) ((c + m) * 255);
+	} else if (h >= 300 && h < 360){
+		color[0] = (uint8_t) ((c + m) * 255);
+		color[1] = (uint8_t) (m * 255);
+		color[2] = (uint8_t) ((x + m) * 255);
+	}
+}
+
+/* TPG error statements */
+static int printError(enum TinyPngOut_Status status){
+	const char *msg;
+	switch (status){
+		case TINYPNGOUT_OK		:	msg = "OK";			break;
+		case TINYPNGOUT_INVALID_ARGUMENT:	msg = "Invalid argument";	break;
+		case TINYPNGOUT_IMAGE_TOO_LARGE	:	msg = "Image too large";	break;
+		case TINYPNGOUT_IO_ERROR	:	msg = "I/O error";		break;
+		default				:	msg = "Unknown error";		break;
+	}
+	fprintf(stderr, "Error: %s\n", msg);
+	return EXIT_FAILURE;
 }
